4aec2f7ad62d0f9ed6ace95ff8d3a355b7dcde3c
Carlos Aruh <carlos.aruh@gmail.com>
2024-02-18 13:56:45 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/carlos/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.carlos.workshopmongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.carlos.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
